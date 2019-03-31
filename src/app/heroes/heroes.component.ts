c7e77fd25ddfc49ad112553931b1676fd85e1675
Shunsuke Okumura <shun.2fb.wb7@gmail.com>
2019-03-31 19:51:26 +0900

    ~Tutorial 6

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'; // Heroクラスをインポート
 // import { HEROES } from '../mock-heroes'; // mock-heroes.tsで定義した配列HEROESをインポート
 import { HeroService } from '../hero.service'; // HEROESのインポートの代わりにHeroServiceでHEROESを取得する
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-heroes',
@@ -20,6 +21,20 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 
 }
